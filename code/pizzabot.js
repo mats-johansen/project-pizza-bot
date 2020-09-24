40dea771069b594d9fedf38c8a585a8be4646077
Mats Johansen <johansenmats89@gmail.com>
2020-09-24 19:02:18 +0200

    Fixed the bug that allowed the costumer to order pizzas with lower value than 0

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,21 +1,24 @@
-const vegetarian = "Vegetarian"
-const hawaiian = "Hawaiian"
-const pepperoni = "Pepperoni"
+const vegetarian = "Vegetarian";
+const hawaiian = "Hawaiian";
+const pepperoni = "Pepperoni";
 const pizzaPrice = 80;
 
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`)
+alert(
+  `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`
+);
 let orderName = prompt('Enter the name of the pizza you want to order today.');
 
 const validateOrderName = () => { 
-    if (orderName.toLowerCase() === vegetarian.toLowerCase() || orderName.toLowerCase() === hawaiian.toLowerCase() || orderName.toLowerCase() === pepperoni.toLowerCase()) { 
-        orderQuantity = prompt(`How many ${orderName} pizzas do you want?`);
-        return true; 
+    if (orderName.toLowerCase() === vegetarian.toLowerCase()
+    || orderName.toLowerCase() === hawaiian.toLowerCase()
+    || orderName.toLowerCase() === pepperoni.toLowerCase()) { 
+        orderQuantity = prompt(`How many ${orderName} pizzas do you want?`); 
     } else {
-        alert("Select a pizza from the menu")
+        alert("Select a pizza from the menu");
         prompt('Enter the name of the pizza you want to order today.');
         validateOrderName();
-    }
-}
+    };
+};
 validateOrderName();
 
 const calculateTotalCost = () => {
@@ -24,14 +27,20 @@ const calculateTotalCost = () => {
 calculateTotalCost();
    
 const cookingTime = () => {
-    if(orderQuantity<2) {
+    if (orderQuantity >0 && orderQuantity <=2) {
       pizzaTime = "10 minutes.";
-    } else if (orderQuantity>=2 && orderQuantity<=5) {
+    } else if ( orderQuantity >2 && orderQuantity <6) {
       pizzaTime = "15 minutes.";
-      } else {
+    } else if ( orderQuantity >=6 && orderQuantity <100) {
       pizzaTime = "20 minutes."
-    }
+    } else {
+      alert(
+        `${orderQuantity} is not a valid order quantity. Please try again and enter a number between 1-100.`
+      );
+    };
 };
 cookingTime();
 
-document.getElementById("pizza-order").innerHTML = (`Greeeaat, I'll get started on your ${orderName} pizza(s) right away, it will cost ${orderTotal} kr. The pizzas will take ${pizzaTime}`)
+document.getElementById("pizza-order").innerHTML = (
+  `Greeeaat, I'll get started on your ${orderName} pizza(s) right away, it will cost ${orderTotal} kr. The pizzas will take ${pizzaTime}`
+);
